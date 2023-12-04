66ad6854510a7ba4a6ea5eecebd09f6c4c17b0bb
Shine Sujith <G00414454@atu.ie>
2023-12-04 16:54:35 +0000

    completed part b

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -6,5 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Person myPerson = new Person("shine", "duyewfdi", "0878");
         System.out.println(myPerson.toString());
+
+        Customer myCustomer = new Customer();
+        Customer myCustomer2 = new Customer("Shine", "efbwwe", "5463526", "236e", true);
+
+        System.out.println(myCustomer2.toString());
     }
 }
