958bea5e64bf4646b3ddd6154dcc78a1dcea06ab
Shine Sujith <G00414454@atu.ie>
2023-12-04 16:33:48 +0000

    Exercise created

---
+++ src/main/java/ie/atu/Main.java
@@ -0,0 +1,10 @@
+package ie.atu;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        Person myPerson = new Person("shine", "duyewfdi", "0878");
+        System.out.println(myPerson.toString());
+    }
+}
